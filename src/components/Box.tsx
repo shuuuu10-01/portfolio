e3167744b5aea4745802ee931adf91e443e5002f
shuuuu10-01 <koron.konan123@gmail.com>
2024-06-13 21:35:27 +0900

    feat: 立方体の大きさをランダムになるように実装

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -2,12 +2,13 @@ import { ThreeElements } from '@react-three/fiber';
 import { Mesh } from 'three';
 
 import useFloating from '../hooks/useFloating';
+import { getRandomBetween } from '../utils/number';
 
 function Box(props: ThreeElements['mesh']) {
   const floatingRef = useFloating<Mesh>();
 
   return (
-    <mesh {...props} ref={floatingRef} scale={20}>
+    <mesh {...props} ref={floatingRef} scale={getRandomBetween(10, 30)}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={'white'} />
     </mesh>
--- src/components/FlameBox.tsx
+++ src/components/FlameBox.tsx
@@ -2,12 +2,13 @@ import { ThreeElements } from '@react-three/fiber';
 import { BoxGeometry, LineSegments } from 'three';
 
 import useFloating from '../hooks/useFloating';
+import { getRandomBetween } from '../utils/number';
 
 function FlameBox(props: ThreeElements['lineSegments']) {
   const floatingRef = useFloating<LineSegments>();
 
   return (
-    <lineSegments {...props} ref={floatingRef} scale={20}>
+    <lineSegments {...props} ref={floatingRef} scale={getRandomBetween(10, 40)}>
       <edgesGeometry args={[new BoxGeometry(1, 1, 1)]} />
       <lineBasicMaterial color={'#fff'} />
     </lineSegments>
