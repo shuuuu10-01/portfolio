11fce6be369979828a05b79f2c1cd4310a8ae48b
shuuuu10-01 <koron.konan123@gmail.com>
2024-06-13 21:10:27 +0900

    feat: 立方体の回転速度がランダムになるように実装

--- src/hooks/useFloating.ts
+++ src/hooks/useFloating.ts
@@ -6,19 +6,16 @@ const height = window.innerHeight;
 
 function useFloating<T extends LineSegments | Mesh>(): MutableRefObject<T> {
   const meshRef = useRef<T>(null!);
-  const heightRandom = Math.random() * 0.5;
+  const initRandom = Math.random();
   useFrame(() => {
-    meshRef.current.rotation.x += 0.01;
-    meshRef.current.rotation.y += 0.01;
+    meshRef.current.rotation.x += 0.01 * initRandom;
+    meshRef.current.rotation.y += 0.01 * initRandom;
     meshRef.current.rotation.z += 0.01;
 
     meshRef.current.position.y += 1.5;
     const material = meshRef.current.material as Material;
 
-    if (meshRef.current.position.y >= height * (0.7 + heightRandom)) {
-      material.opacity -= 0.1; // 透明度を徐々に下げる
-    }
-    if (material.opacity <= 0) {
+    if (meshRef.current.position.y >= height) {
       meshRef.current.position.x = window.innerWidth * (Math.random() - 0.5);
       meshRef.current.position.y = 0;
       meshRef.current.position.z = window.innerWidth * (Math.random() - 0.5);
