13ff3518e16c1eb749585a6cb60d3dd29d7feac6
shuuuu10-01 <koron.konan123@gmail.com>
2024-06-13 23:03:59 +0900

    add: chakraUIの導入

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 import './index.css';
+import ChakraProvider from './plugins/ChakraProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 );
---
+++ src/plugins/ChakraProvider.tsx
@@ -0,0 +1,34 @@
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/theme/components';
+import { ReactNode } from 'react';
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+  semanticTokens: {
+    colors: {
+      error: 'red.500',
+      text: {
+        default: 'gray.900',
+      },
+    },
+    styles: {
+      global: {
+        body: {
+          bg: '#242424',
+        },
+      },
+    },
+  },
+});
+
+function ChakraProvider({ children }: { children: ReactNode }) {
+  return (
+    <ChakraBaseProvider resetCSS theme={theme}>
+      {children}
+    </ChakraBaseProvider>
+  );
+}
+
+export default ChakraProvider;
