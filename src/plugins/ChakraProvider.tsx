f88c32325fed8ec13cf509c4ae357fb69180d63a
shuuuu10-01 <koron.konan123@gmail.com>
2024-06-13 23:58:38 +0900

    feat: カラー定義追加

--- src/plugins/ChakraProvider.tsx
+++ src/plugins/ChakraProvider.tsx
@@ -1,13 +1,15 @@
-import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
-import { Button } from '@chakra-ui/theme/components';
+import { ChakraBaseProvider, extendBaseTheme, Button, IconButton, Icon } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 
 const theme = extendBaseTheme({
   components: {
     Button,
+    IconButton,
+    Icon,
   },
   semanticTokens: {
     colors: {
+      primary: '#3997d1',
       error: 'red.500',
       text: {
         default: 'gray.900',
