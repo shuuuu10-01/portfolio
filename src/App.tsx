a2905af4fce6ccd7a65c52826b8a198f5c4a9c97
shuuuu10-01 <koron.konan123@gmail.com>
2024-06-14 00:04:31 +0900

    feat: footerの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/Footer';
 import Scene from './components/Scene';
 
 function App() {
   return (
     <div className="container">
       <Scene />
+      <Footer />
     </div>
   );
 }
